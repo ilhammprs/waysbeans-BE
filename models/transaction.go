04b3aea191925f1c486a18290639e7c82d49dfe6
Ilham <ilhamprastiyi345@gmail.com>
2022-09-07 23:08:28 +0700

    update

--- models/transaction.go
+++ models/transaction.go
@@ -3,7 +3,6 @@ package models
 import "time"
 
 type Transaction struct {
-	ID        int64     `json:"id"`
 	UserID    int       `json:"user_id"`
 	User      User      `json:"user"`
 	Status    string    `json:"status"`
